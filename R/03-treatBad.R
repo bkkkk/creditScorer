bd27e23b48f601d363bdc8c61781a1cbed93fb5f
Jacobo Blanco <jayb88@gmail.com>
2016-10-30 18:33:29 +0900

    Add file to treat bad values and NAs

---
+++ R/03-treatBad.R
@@ -0,0 +1,4 @@
+imputed_tr <- clean_credit_score_age(clean_tr)
+imputed_tr <- clean_credit_score_pastDue30(imputed_tr)
+imputed_tr <- clean_credit_score_pastDue60(imputed_tr)
+imputed_tr <- clean_credit_score_pastDue90(imputed_tr)
