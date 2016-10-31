da7471aa7f8f67838f20c6ceaf6d019fc644217f
Jacobo Blanco <jayb88@gmail.com>
2016-10-31 22:16:38 +0900

    Remove missing entries from training data

--- R/03-treatBad.R
+++ R/03-treatBad.R
@@ -4,6 +4,8 @@ imputed_tr <- clean_credit_score_age(clean_tr)
 imputed_tr <- clean_credit_score_pastDue30(imputed_tr)
 imputed_tr <- clean_credit_score_pastDue60(imputed_tr)
 imputed_tr <- clean_credit_score_pastDue90(imputed_tr)
+imputed_tr <- na.omit(imputed_tr)
+
 imputed_te <- clean_credit_score_age(clean_te)
 imputed_te <- clean_credit_score_pastDue30(imputed_te)
 imputed_te <- clean_credit_score_pastDue60(imputed_te)
