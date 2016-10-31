b2ef85e0ef38d48acbb8d49c3399bde958381a56
Jacobo Blanco <jayb88@gmail.com>
2016-10-31 22:16:27 +0900

    Add loading and cleaning of testing data

--- R/01-load.R
+++ R/01-load.R
@@ -2,3 +2,6 @@ library(readr)
 
 raw_tr <- read_csv("data/cs-training.csv")
 raw_tr <- raw_tr[,-1]
+
+raw_te <- read_csv("data/cs-test.csv")
+raw_te <- raw_tr[,-1]
--- R/02-clean.R
+++ R/02-clean.R
@@ -3,3 +3,4 @@ source("R/func-clean.R")
 clean_tr <- clean_credit_score_data(raw_tr)
 clean_tr <- clean_credit_score_training(clean_tr)
 
+clean_te <- clean_credit_score_data(raw_te)
